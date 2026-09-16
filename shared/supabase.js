627ba70e67d2e84300507512b26829b10972cc47
yasser-ezzahri-g4c <yasser.ezzahri@grs4cloud.com>
2026-09-16 19:06:55 +0100

    Update supabase.js

--- shared/supabase.js
+++ shared/supabase.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = "https://nvajehtowtehymgpqwvn.supabase.co";
-export const SUPABASE_KEY = "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52YWplaHRvd3RlaHltZ3Bxd3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NjQ5MTksImV4cCI6MjEwNTE0MDkxOX0.7cnl3q4sBzQjdWtYm2n8DavGvhP1OjHLFPwruPJ6Uos";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52YWplaHRvd3RlaHltZ3Bxd3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NjQ5MTksImV4cCI6MjEwNTE0MDkxOX0.7cnl3q4sBzQjdWtYm2n8DavGvhP1OjHLFPwruPJ6Uos";
 
 export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
